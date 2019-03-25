aabe639b0c83aac827e6f289bfec42395e424c9f
Jan Chaloupka <janch32@gmail.com>
2019-03-25 20:43:07 +0100

    Add project files.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CachedFilesExtractor
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.ProfileSelect = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Stats = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Stats)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.BackColor = System.Drawing.Color.LightGray;
+            this.SaveButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SaveButton.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.SaveButton.FlatAppearance.BorderSize = 2;
+            this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveButton.Location = new System.Drawing.Point(196, 13);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(87, 30);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Uložit";
+            this.SaveButton.UseVisualStyleBackColor = false;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButtonClick);
+            // 
+            // ProfileSelect
+            // 
+            this.ProfileSelect.BackColor = System.Drawing.Color.Gainsboro;
+            this.ProfileSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ProfileSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProfileSelect.FormattingEnabled = true;
+            this.ProfileSelect.ItemHeight = 17;
+            this.ProfileSelect.Location = new System.Drawing.Point(12, 17);
+            this.ProfileSelect.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ProfileSelect.Name = "ProfileSelect";
+            this.ProfileSelect.Size = new System.Drawing.Size(172, 25);
+            this.ProfileSelect.TabIndex = 3;
+            this.ProfileSelect.SelectedValueChanged += new System.EventHandler(this.ProfileSelectChange);
+            // 
+            // label1
+            // 
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.Text = "Připraveno";
+            // 
+            // Stats
+            // 
+            this.Stats.Location = new System.Drawing.Point(12, 73);
+            this.Stats.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Stats.Name = "Stats";
+            this.Stats.Size = new System.Drawing.Size(271, 356);
+            this.Stats.TabIndex = 6;
+            this.Stats.TabStop = false;
+            this.Stats.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawStats);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(295, 442);
+            this.Controls.Add(this.Stats);
+            this.Controls.Add(this.ProfileSelect);
+            this.Controls.Add(this.SaveButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Cached Files Extractor";
+            this.Load += new System.EventHandler(this.WindowLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.Stats)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.ToolStripStatusLabel label1;
+        private System.Windows.Forms.ComboBox ProfileSelect;
+        private System.Windows.Forms.PictureBox Stats;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+
+namespace CachedFilesExtractor
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static string ChromeUserDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Google\\Chrome\\User Data");
+        string[] SourceFiles = null;
+        int SourceFilesIndex = 0;
+        Dictionary<string, int> ParsedFormats = null;
+
+
+		void LoadChromeProfiles()
+		{
+			ProfileSelect.Items.Clear();
+
+			foreach (string folders in Directory.GetDirectories(ChromeUserDataPath))
+			{
+				if (Directory.Exists(Path.Combine(folders, "Cache")))
+				{
+					ProfileSelect.Items.Add(Path.GetFileName(folders));
+				}
+			}
+
+			ProfileSelect.SelectedIndex = 0;
+		}
+
+        static ImageFormat FileTryGetImageFormat(string filename)
+        {
+            try
+            {
+                var img = Image.FromFile(filename);
+                var format = img.RawFormat;
+                img.Dispose();
+                return format;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+		void AllFilesLoaded()
+		{
+			Invoke(new Action(() => {
+				ProfileSelect.Enabled = true;
+				SaveButton.Enabled = true;
+			}));
+		}
+
+		void InvalidateStats()
+		{
+			Invoke(new Action(() => {
+				Stats.Invalidate();
+			}));
+		}
+
+        static ImageFormatConverter FormatConverter = new ImageFormatConverter();
+
+        private void GetFilesFromCache()
+        {
+			try
+            {
+			    string OutputFolder = "Záloha cache - " + SelectedProfile + " (" + DateTime.Now.ToFileTime() + ")";
+                SourceFiles = Directory.GetFiles(Path.Combine(ChromeUserDataPath, SelectedProfile, "Cache"));
+                ParsedFormats = new Dictionary<string, int>();
+
+                Directory.CreateDirectory(OutputFolder);
+
+                for (SourceFilesIndex = 0; SourceFilesIndex < SourceFiles.Length; SourceFilesIndex++)
+                {
+                    string SourcePath = SourceFiles[SourceFilesIndex];
+                    var Format = FileTryGetImageFormat(SourcePath);
+
+                    if (Format != null)
+                    {
+                        string FormatExtension = FormatConverter.ConvertToString(Format).ToLower();
+
+                        string OutputFilename = "Obrázek " + ParsedFormats.Values.Sum() + "." + FormatExtension;
+                        File.Copy(SourcePath, Path.Combine(OutputFolder, OutputFilename));
+
+                        if (!ParsedFormats.ContainsKey(FormatExtension))
+                            ParsedFormats.Add(FormatExtension, 0);
+
+                        ParsedFormats[FormatExtension]++;
+                    }
+
+                    InvalidateStats();
+                }
+            }
+            catch (Exception) {
+                // chyba při IO přístupu
+            }
+
+            AllFilesLoaded();
+        }
+
+        private void SaveButtonClick(object sender, EventArgs e)
+        {
+			ProfileSelect.Enabled = false;
+			SaveButton.Enabled = false;
+            
+			Thread t = new Thread(new ThreadStart(GetFilesFromCache));
+            t.Start();
+        }
+
+        private static Brush GetBrushByFormat(string format)
+        {
+            if (format.Equals("bmp")) return Brushes.Blue;
+            if (format.Equals("emf")) return Brushes.Red;
+            if (format.Equals("exif")) return Brushes.Green;
+            if (format.Equals("gif")) return Brushes.Orange;
+            if (format.Equals("ico")) return Brushes.Purple;
+            if (format.Equals("jpeg")) return Brushes.Pink;
+            if (format.Equals("png")) return Brushes.LimeGreen;
+            if (format.Equals("tiff")) return Brushes.Azure;
+            if (format.Equals("wmf")) return Brushes.Brown;
+            return Brushes.Gray;
+        }
+
+        private void DrawStats(object sender, PaintEventArgs e)
+        {
+            Control c = (Control) sender;
+            Graphics g = e.Graphics;
+            int center = (c.Width - 150) / 2;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            g.FillPie(Brushes.WhiteSmoke, center, 6, 150, 150, -90, 360);
+
+            if (SourceFiles == null || SourceFiles.Length == 0 || ParsedFormats == null) return;
+            float progress = (float)(SourceFilesIndex + 1) / SourceFiles.Length;
+
+            g.FillPie(Brushes.LightGray, center, 6, 150, 150, -90, progress * 360);
+                
+            if (ParsedFormats.Values.Count > 0)
+            {
+                float celkem = -90;
+                foreach (var f in ParsedFormats.Keys)
+                {
+                    float angle = (float)ParsedFormats[f] / SourceFiles.Length * 360;
+                    g.FillPie(GetBrushByFormat(f), center, 0, 150, 150, celkem, angle);
+                    celkem += angle;
+                }
+            }
+
+            // Legenda 
+
+
+            g.FillRectangle(Brushes.LightGray, 20, c.Height - DefaultFont.Height, 10, 10);
+            g.DrawString((SourceFilesIndex + 1) + " prohledaných souborů", DefaultFont, Brushes.Black, 40, c.Height - DefaultFont.Height);
+
+            int i = 1;
+            foreach (var f in ParsedFormats.Keys)
+            {
+                i++;
+                g.FillRectangle(GetBrushByFormat(f), 20, c.Height - i * DefaultFont.Height, 10, 10);
+                g.DrawString(ParsedFormats[f] + " " + f + " obrázků", DefaultFont, Brushes.Black, 40, c.Height - i * DefaultFont.Height);
+            }
+        }
+
+        private void WindowLoad(object sender, EventArgs e)
+        {
+            LoadChromeProfiles();
+            if (ProfileSelect.Items.Count < 1)
+            {
+                MessageBox.Show("Žádné profily prohlížeče Google Chrome nenalezeny", "CacheToFile", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Close();
+            }
+        }
+
+        string SelectedProfile = "";
+        private void ProfileSelectChange(object sender, EventArgs e)
+        {
+            SelectedProfile = (string)((ComboBox)sender).SelectedItem;
+        }
+    }
+}
